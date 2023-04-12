85016105c6ccde148532641ae74824cee73759cd
Luca Schmid <Luca.Schmid@nttdata.com>
2023-04-12 19:54:09 +0200

    Basic Post Component using bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,9 @@
-
-import { helloWorld } from '@pages/Feed'
 import Feed from '@pages/Feed/component'
 
 function App (): JSX.Element {
   return (
     <>
       <Feed></Feed>
-      {helloWorld}
     </>
   )
 }
--- frontend/src/pages/feed/component.tsx
+++ frontend/src/pages/feed/component.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 
-export const helloWorld: string = 'Hello World'
+import { Post } from '@pages/Feed/Post'
+import type { postInterface } from '@pages/Feed/Post/interface'
 
 // ToDo: Fetch Database
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const [feedItems, setFeedItems] = useState(() => {
+/* const [feedItems, setFeedItems] = useState(() => {
   const localValue = localStorage.getItem('ITEMS')
   if (localValue == null) return []
 
@@ -14,11 +15,43 @@ const [feedItems, setFeedItems] = useState(() => {
 useEffect(() => {
   localStorage.setItem('ITEMS', JSON.stringify(feedItems))
 }, [feedItems])
+*/
+
+const posts: postInterface[] = [
+  {
+    id: crypto.randomUUID(),
+    title: 'Hey Ben',
+    artist: 'Hoodie Allen',
+    album: 'Games we play',
+    imgUrl:
+      'https://images.unsplash.com/photo-1612999754243-3745bba6c302?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=734&q=80'
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Happy',
+    artist: 'Cro',
+    album: 'test',
+    imgUrl:
+      'https://images.unsplash.com/photo-1620939123578-bb16277cfd54?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80'
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Happy',
+    artist: 'Cro',
+    album: 'test',
+    imgUrl:
+      'https://images.unsplash.com/photo-1612999754243-3745bba6c302?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=734&q=80'
+  }
+]
 
 export default function Feed (): JSX.Element {
   return (
     <>
-
+      {posts.map((post) => (
+        <>
+          <Post {...post}> </Post>
+        </>
+      ))}
     </>
   )
 }
