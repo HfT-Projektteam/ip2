abc024c6badec49d282af520c721173dff1bdc40
thomasjakkel <73285969+thomasjakkel@users.noreply.github.com>
2023-04-13 19:04:42 +0200

    Jest config

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const CracoAlias = require('craco-alias')
+const { pathsToModuleNameMapper } = require('ts-jest')
+const { compilerOptions } = require('./tsconfig.paths.json')
 
 module.exports = {
   plugins: [
@@ -8,9 +10,9 @@ module.exports = {
       options: {
         source: 'tsconfig',
         baseUrl: './src',
-        tsConfigPath: './tsconfig.paths.json'
-      }
-    }
+        tsConfigPath: './tsconfig.paths.json',
+      },
+    },
   ],
   webpack: {
     alias: {
@@ -20,7 +22,15 @@ module.exports = {
       data: path.resolve(__dirname, 'src/data'),
       hooks: path.resolve(__dirname, 'src/hooks'),
       pages: path.resolve(__dirname, 'src/pages'),
-      services: path.resolve(__dirname, 'src/services')
-    }
-  }
+      services: path.resolve(__dirname, 'src/services'),
+    },
+  },
+  jest: {
+    configure: {
+      preset: 'ts-jest',
+      moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+        prefix: 'frontend/src/',
+      }),
+    },
+  },
 }
