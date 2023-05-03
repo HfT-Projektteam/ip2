9b4bb2d418945ec07002cadbf11fd87a752100bd
Benny Burkert <benny.burkert@codecentric.de>
2023-05-03 22:12:41 +0200

    Add openapi generator

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { EntityNotFoundExceptionFilter } from './filters/entity-not-found-exception/entity-not-found-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new EntityNotFoundExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('Friendify Backend Service')
+    .setDescription('')
+    .setVersion('1.0')
+    .addTag('spotify')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  fs.writeFileSync('./openapi-spec.json', JSON.stringify(document, null, 2));
+
   await app.listen(3000);
 }
 bootstrap();
