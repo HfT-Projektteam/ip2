70075fbd144f00a282fdcf1badff1f2f45942bb3
thomasjakkel <73285969+thomasjakkel@users.noreply.github.com>
2023-05-25 18:30:09 +0200

    Added global responsive  mobile container

--- frontend/src/pages/Feed/Post/component.tsx
+++ frontend/src/pages/Feed/Post/component.tsx
@@ -5,25 +5,11 @@ const { Meta } = Card
 
 export function Post(props: postInterface): JSX.Element {
   return (
-    <Card
-      style={{ width: '300px' }}
-      cover={<img alt='example' src={props.imgUrl} />}
-    >
+    <Card cover={<img alt='example' src={props.imgUrl} />}>
       <Meta
         title={props.title}
         description={`${props.album} \n ${props.artist}`}
       />
     </Card>
-
-    /* old bootstrap example
-    <Card key={props.id}>
-      <Card.Img variant='top' src={props.imgUrl} />
-      <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text>{props.album}</Card.Text>
-        <Card.Text>{props.artist}</Card.Text>
-      </Card.Body>
-    </Card>
-    */
   )
 }
