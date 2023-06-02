97d6918a8988e264475269e1d80a8020a0af91fb
thomasjakkel <73285969+thomasjakkel@users.noreply.github.com>
2023-06-03 01:05:35 +0200

    Added tests for routing Checking dynamic url path when button is clicked

--- frontend/src/components/ui/NavBar/.test.tsx
+++ frontend/src/components/ui/NavBar/.test.tsx
@@ -1,6 +1,7 @@
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import NavBar from '@Components/ui/NavBar'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, MemoryRouter, Route, Router } from 'react-router-dom'
+import { createMemoryHistory } from 'history'
 
 describe('NavBar', () => {
   it('should render Navigation Bar correctly with 4 buttons', () => {
@@ -9,5 +10,46 @@ describe('NavBar', () => {
     })
     const buttons = getAllByRole('button')
     expect(buttons).toHaveLength(4)
+  })
+
+  it('should route to a new route when a button is clicked', async () => {
+    const history = createMemoryHistory()
+
+    // mock push function
+    history.push = jest.fn()
+
+    const { getAllByRole } = render(
+      <Router location={history.location} navigator={history}>
+        <NavBar />
+      </Router>,
+    )
+    const buttons = getAllByRole('button')
+    expect(buttons).toHaveLength(4)
+
+    const profileButton = buttons.at(3)
+    profileButton && fireEvent.click(profileButton)
+
+    // spy on push calls, assert on url (parameter)
+    expect(history.push).toHaveBeenCalledWith(
+      {
+        hash: '',
+        pathname: '/profile',
+        search: '',
+      },
+      undefined,
+      {},
+    )
+
+    const feedButton = buttons.at(0)
+    feedButton && fireEvent.click(feedButton)
+    expect(history.push).toHaveBeenCalledWith(
+      {
+        hash: '',
+        pathname: '/feed',
+        search: '',
+      },
+      undefined,
+      {},
+    )
   })
 })
