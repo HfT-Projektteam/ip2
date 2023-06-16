0b82f6ef1c319f71d2bb84cb3cd52fe8e1ad465c
LucaSchmid <52951159+LucaSchmid@users.noreply.github.com>
2023-06-16 11:28:10 +0200

    Switched Button

--- frontend/src/pages/Feed/Post/component.tsx
+++ frontend/src/pages/Feed/Post/component.tsx
@@ -1,10 +1,10 @@
-import { PlusCircleOutlined } from '@ant-design/icons'
+import { PlusOutlined } from '@ant-design/icons'
 import {
   type trackInterface,
   type postInterface,
 } from '@pages/Feed/Post/interface'
 import { addSongToPlaylist, getTrack } from '@services/SpotifyAPI'
-import { Card } from 'antd'
+import { Button, Card } from 'antd'
 import { useEffect, useState } from 'react'
 
 const { Meta } = Card
@@ -26,14 +26,16 @@ export function Post(props: postInterface): JSX.Element {
     <Card
       cover={<img alt='example' src={post?.imgUrl} />}
       actions={[
-        <PlusCircleOutlined
-          rev='' // WARUM muss das da sein????
-          key={'addSongToPlaylist' + String(post?.id)}
-          // eslint-disable-next-line @typescript-eslint/no-misused-promises
-          onClick={async () => {
-            await addSongToPlaylist(post?.spotifyId ?? '')
-          }}
-        />,
+        <>
+          <Button
+            type='primary'
+            shape='circle'
+            icon={<PlusOutlined rev='AddSongToPlaylist' />}
+            onClick={() => {
+              void addSongToPlaylist(post?.spotifyId ?? '')
+            }}
+          />
+        </>,
       ]}
     >
       <Meta
