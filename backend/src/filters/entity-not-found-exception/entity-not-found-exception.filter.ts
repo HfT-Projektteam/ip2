a3211924b602625cf72da73ab8d84398527984b5
Benny Burkert <benny.burkert@codecentric.de>
2023-06-18 16:10:09 +0200

    Add filter test

--- backend/src/filters/entity-not-found-exception/entity-not-found-exception.filter.ts
+++ backend/src/filters/entity-not-found-exception/entity-not-found-exception.filter.ts
@@ -1,6 +1,6 @@
-import { Catch, ExceptionFilter, ArgumentsHost } from '@nestjs/common';
-import { EntityNotFoundError } from 'typeorm/error/EntityNotFoundError';
-import { Response } from 'express';
+import { Catch, ExceptionFilter, ArgumentsHost } from '@nestjs/common'
+import { EntityNotFoundError } from 'typeorm/error/EntityNotFoundError'
+import { Response } from 'express'
 
 /**
  * Custom exception filter to convert EntityNotFoundError from TypeOrm to NestJs responses
@@ -9,16 +9,14 @@ import { Response } from 'express';
 @Catch(EntityNotFoundError)
 export class EntityNotFoundExceptionFilter implements ExceptionFilter {
   public catch(exception: EntityNotFoundError, host: ArgumentsHost) {
-    const ctx = host.switchToHttp();
-    const response = ctx.getResponse<Response>();
-    return response
-      .status(404)
-      .json({
-        message: {
-          statusCode: 404,
-          error: 'Not Found',
-          message: exception.message,
-        },
-      });
+    const ctx = host.switchToHttp()
+    const response = ctx.getResponse<Response>()
+    return response.status(404).json({
+      message: {
+        statusCode: 404,
+        error: 'Not Found',
+        message: exception.message,
+      },
+    })
   }
 }
