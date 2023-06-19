bc385e1b27703b7c14914276932fef02103ce4ea
LucaSchmid <52951159+LucaSchmid@users.noreply.github.com>
2023-06-19 22:46:19 +0200

    Search User tests

--- frontend/src/pages/Search/.test.tsx
+++ frontend/src/pages/Search/.test.tsx
@@ -1,6 +1,41 @@
+import { act, render, screen } from '@testing-library/react'
+import { SearchPage } from './component'
+import userEvent from '@testing-library/user-event'
+
 jest.mock('@services/SpotifyAPI')
 jest.mock('@services/BackendAPI')
 
 describe('Search', () => {
-  it.todo('should render Search correctly')
+  it('should have all Components', async () => {
+    act(() => render(<SearchPage />))
+
+    await act(async () => {
+      // Wait for the update in NewPost to complete
+      await new Promise((resolve) => setTimeout(resolve, 0))
+    })
+
+    const textarea = await screen.findAllByRole('textbox')
+
+    expect(textarea).toHaveLength(1)
+  })
+
+  it('should find the users', async () => {
+    act(() => render(<SearchPage />))
+
+    await act(async () => {
+      // Wait for the update in NewPost to complete
+      await new Promise((resolve) => setTimeout(resolve, 0))
+    })
+
+    const searchBox = await screen.findByPlaceholderText('Search User')
+
+    await act(async () => {
+      userEvent.type(searchBox, 'User Name{enter}')
+    })
+
+    await new Promise((resolve) => setTimeout(resolve, 0))
+
+    const followerList = await screen.findAllByRole('listitem')
+    expect(followerList).toHaveLength(4)
+  })
 })
