4fc854cca8631af5786408407a48c64864b890e5
thomasjakkel <73285969+thomasjakkel@users.noreply.github.com>
2023-06-19 16:33:29 +0200

    Implemented Dynamic headers for different paths

--- frontend/src/components/layout/Header/component.tsx
+++ frontend/src/components/layout/Header/component.tsx
@@ -7,27 +7,33 @@ import { useLocation } from 'react-router-dom'
 export const Header = (): JSX.Element => {
   const location = useLocation()
 
-  const Test: JSX.Element =
-    location.pathname === '/feed' ? <FeedHeader /> : <Settings />
+  const headersContent: Array<{ path: string; node: JSX.Element }> = [
+    { path: 'feed', node: <FeedHeader /> },
+    { path: 'profile', node: <ProfileHeader /> },
+  ]
+
+  const headerContentObject = headersContent.find(
+    (item) => '/' + item.path === location.pathname,
+  )
+
+  const headerContent =
+    headerContentObject != null ? headerContentObject.node : <></>
 
   return (
     <>
       <p>{'Header'}</p>
       <br />
-      {Test}
+      {headerContent}
     </>
   )
 }
 
-const Settings = (): JSX.Element => {
+const ProfileHeader = (): JSX.Element => {
   const { isDarkmode, changeTheme } = useTheme()
   const { handleLogout } = useAuth()
 
   return (
     <>
-      <Button type='primary' size='large' onClick={changeTheme}>
-        Switch Theme
-      </Button>
       <Switch
         checkedChildren={<CheckOutlined rev={undefined} />}
         unCheckedChildren={<CloseOutlined rev={undefined} />}
