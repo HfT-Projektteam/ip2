3dd4e6896d1a47ebb846c8882171e59bed88818c
LucaSchmid <52951159+LucaSchmid@users.noreply.github.com>
2023-06-21 21:44:58 +0200

    infinit feed

--- frontend/src/pages/Feed/component.tsx
+++ frontend/src/pages/Feed/component.tsx
@@ -4,7 +4,8 @@ import { Post } from '@pages/Feed/Post'
 import { type HandleFeedChange, type feedInterface } from './interface'
 import { useEffect, useState } from 'react'
 import { getPosts } from '@services/BackendAPI/component'
-import { Button } from 'antd'
+import { Button, List } from 'antd'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 interface FeedProps {
   feed: feedInterface
@@ -14,36 +15,72 @@ interface FeedProps {
 export function Feed({ feed, handleFeedChange }: FeedProps): JSX.Element {
   const [isPrivateFeed, setIsPrivateFeed] = useState(true)
   const [pagination, setPagination] = useState(0)
+  const [loading, setLoading] = useState(false)
 
   const switchFeed = (): void => {
+    setPagination(0)
+    handleFeedChange({ posts: [] })
     setIsPrivateFeed(!isPrivateFeed)
   }
+
+  useEffect(() => {
+    setPagination(0)
+    return (): void => {
+      handleFeedChange({ posts: [] })
+    }
+  }, [])
 
   useEffect(() => {
     void (async () => {
       const allPosts = await getPosts('', isPrivateFeed, undefined, pagination)
       if (allPosts === null) return
 
-      const newFeed = allPosts?.map((post) => {
+      const newPosts = allPosts?.map((post) => {
         return { id: post.uuid, spotifyId: post.songId }
       })
 
+      const newFeed = feed.posts.concat(newPosts)
+
       handleFeedChange({ posts: newFeed })
+      setLoading(false)
     })()
-  }, [handleFeedChange, pagination, isPrivateFeed])
+  }, [pagination, isPrivateFeed])
+
+  const loadMoreData = (): void => {
+    if (loading) return
+    setPagination(pagination === undefined ? 0 : pagination + 1)
+    setLoading(true)
+  }
   return (
     <>
-      <Button
-        onClick={() => {
-          setPagination(pagination + 1)
-        }}
-      >
-        Fetch next 10
-      </Button>
+      {feed.posts.length}
       <Button onClick={switchFeed}>Switch Feed (Global/Private)</Button>
-      {feed.posts.map((post) => (
-        <Post key={post.id} {...post}></Post>
-      ))}
+
+      <InfiniteScroll
+        dataLength={feed.posts.length}
+        next={() => {
+          loadMoreData()
+        }}
+        hasMore={true}
+        loader={<></>}
+        scrollableTarget='scrollableDiv'
+      >
+        <List
+          style={{
+            padding: '32px 10px 0px 10px',
+            minHeight: '80vh',
+          }}
+          key='feed.list'
+          itemLayout='horizontal'
+          dataSource={feed.posts}
+          locale={{ emptyText: ' ' }}
+          renderItem={(post) => (
+            <List.Item>
+              <Post key={post.id} {...post} />
+            </List.Item>
+          )}
+        />
+      </InfiniteScroll>
     </>
   )
 }
