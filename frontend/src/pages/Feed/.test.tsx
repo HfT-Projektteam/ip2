6037f3674bca2913d791c265f955b6b71dade713
thomasjakkel <73285969+thomasjakkel@users.noreply.github.com>
2023-06-22 14:42:41 +0200

    test

--- frontend/src/pages/Feed/.test.tsx
+++ frontend/src/pages/Feed/.test.tsx
@@ -1,13 +1,25 @@
 import { Feed } from './component'
 import { act, render, screen } from '@testing-library/react'
 import feedMock from '@data/mockdata/feed.json'
+import { BrowserRouter } from 'react-router-dom'
 
 jest.mock('@services/SpotifyAPI')
 jest.mock('@services/BackendAPI')
+jest.spyOn(window, 'scrollTo')
 
 describe('Feed', () => {
   it('should render Feed correctly', async () => {
-    act(() => render(<Feed feed={feedMock} handleFeedChange={() => {}} />))
+    act(() =>
+      render(
+        <Feed
+          feed={feedMock}
+          handleFeedChange={() => {}}
+          genre={''}
+          sort={'newest'}
+        />,
+        { wrapper: BrowserRouter },
+      ),
+    )
 
     await act(async () => {
       // Wait for the update in Post to complete
